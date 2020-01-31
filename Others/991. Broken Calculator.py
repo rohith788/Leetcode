52c66b1c7ad18ebb1d218706b590cab6ec35a875
rohith788 <srohith788@gmail.com>
2020-01-30 21:27:12 -0500

    medium but extremly easy

---
+++ Others/991. Broken Calculator.py
@@ -0,0 +1,10 @@
+class Solution:
+    def brokenCalc(self, X: int, Y: int) -> int:
+        count = 0
+        while Y > X:
+          count += 1
+          if Y % 2 == 1:
+            Y += 1
+          else:
+            Y //= 2
+        return count + X - Y
