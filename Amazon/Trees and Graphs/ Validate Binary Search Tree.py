c39d2855b5c1186a6a1f72d7f4a637d4f51cf2af
rohith788 <srohith788@gmail.com>
2020-08-13 12:29:18 -0400

    iterative approach

--- Amazon/Trees and Graphs/ Validate Binary Search Tree.py
+++ Amazon/Trees and Graphs/ Validate Binary Search Tree.py
@@ -6,14 +6,21 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: TreeNode) -> bool:
-        return self.traverse(root)
-        
-    def traverse(self, node, left=float('-inf'), right=float('inf')):
-        if not node: return True
-        print(node.val, left, right)
-        if node.val <= left or node.val >= right: return False
-        if not self.traverse(node.left, left, node.val): 
-            return False
-        if not self.traverse(node.right, node.val, right): 
-            return False
+        # def traverse(node, left=float('-inf'), right=float('inf')):
+        #     if node is None: return True
+        #     if node.val <= left or node.val >= right: return False
+        #     if not traverse(node.left, left, node.val): return False
+        #     if not traverse(node.right, node.val, right):return False
+        #     return True
+        left=float('-inf')
+        right=float('inf')
+        stack = [(root, left, right)]
+        while stack:
+            node, left, right = stack.pop()
+            if not node: continue
+            if node.val <= left or node.val >= right: return False
+            stack.append((node.left, left, node.val))
+            stack.append((node.right, node.val, right))
         return True
+        
+    
