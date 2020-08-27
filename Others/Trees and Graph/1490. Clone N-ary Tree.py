d66c6ead629f5e029b704b97ca506b7ec041f2ce
rohith788 <srohith788@gmail.com>
2020-08-27 12:41:15 -0400

    q1 of the day

---
+++ Others/Trees and Graph/1490. Clone N-ary Tree.py
@@ -0,0 +1,12 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def cloneTree(self, root: 'Node') -> 'Node':
+
+        return Node(root.val, [self.cloneTree(child) for child in root.children]) if root else None
